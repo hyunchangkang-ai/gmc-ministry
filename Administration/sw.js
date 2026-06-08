1d0848350b30a38ca91d6cb6b11c4621a067a8c4
Hyunchang Kang <hyunchangkang@Hyunchangs-Mac-mini.local>
2026-06-08 07:35:42 -0400

    Bump PWA version to 17: merge duplicate todos inside mergeDuplicateCalMinistries

--- Administration/sw.js
+++ Administration/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gmc-v16';
+const CACHE = 'gmc-v17';
 const STATIC_ASSETS = [
   '/gmc-ministry/Administration/icons/icon-192.png',
   '/gmc-ministry/Administration/icons/icon-512.png',

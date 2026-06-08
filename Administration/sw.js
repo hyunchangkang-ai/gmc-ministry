a7e5857910eb856ad850e4531264968c137ce76e
Hyunchang Kang <hyunchangkang@Hyunchangs-Mac-mini.local>
2026-06-08 07:24:17 -0400

    Bump PWA version to 15: normalize and merge primary calendar duplicate entries

--- Administration/sw.js
+++ Administration/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gmc-v14';
+const CACHE = 'gmc-v15';
 const STATIC_ASSETS = [
   '/gmc-ministry/Administration/icons/icon-192.png',
   '/gmc-ministry/Administration/icons/icon-512.png',
